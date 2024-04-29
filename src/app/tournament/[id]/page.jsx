f49ecd6ec7e7169330c31598cfc1e54b8c3488d7
Xavi Perez Fondevila <66711995+XavierPerezFondevila@users.noreply.github.com>
2024-04-29 21:57:40 +0200

    Update page.jsx

--- src/app/tournament/[id]/page.jsx
+++ src/app/tournament/[id]/page.jsx
@@ -5,11 +5,12 @@ import { Suspense } from "react";
 export default function Page({ params }) {
   return (
     <>
-      <h1 className="title text-center mb-4">Datos del torneo</h1>
+      <p>{params.id}</p>
+      {/* <h1 className="title text-center mb-4">Datos del torneo</h1>
       <Suspense fallback={`Cargando...`}>
         <TournamentView tournamentId={params.id} />
         <TournamentViewBottom tournamentId={params.id} />
-      </Suspense>
+      </Suspense> */}
     </>
   );
 }
