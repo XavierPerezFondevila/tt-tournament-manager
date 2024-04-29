ef17f1de96426e74434160a2709ca87b99fe6b58
Xavi Perez Fondevila <66711995+XavierPerezFondevila@users.noreply.github.com>
2024-04-29 22:58:15 +0200

    params test

--- src/app/tournament/[id]/page.jsx
+++ src/app/tournament/[id]/page.jsx
@@ -3,14 +3,14 @@ import TournamentViewBottom from "@/app/components/tournamentView/tournamentView
 import { Suspense } from "react";
 
 export default function Page({ params }) {
-  return (
-    <>
-      <p>{params.id}</p>
-      {/* <h1 className="title text-center mb-4">Datos del torneo</h1>
-      <Suspense fallback={`Cargando...`}>
-        <TournamentView tournamentId={params.id} />
-        <TournamentViewBottom tournamentId={params.id} />
-      </Suspense> */}
-    </>
-  );
+  return <p>{params.id}</p>;
+  //   <>
+
+  //     {/* <h1 className="title text-center mb-4">Datos del torneo</h1>
+  //     <Suspense fallback={`Cargando...`}>
+  //       <TournamentView tournamentId={params.id} />
+  //       <TournamentViewBottom tournamentId={params.id} />
+  //     </Suspense> */}
+  //   </>
+  // );
 }
